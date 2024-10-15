fe01672c5ef0159bfbae50df847c05257548dad9
Daniel1017 <daniel_saenz@soy.sena.edu.co>
2024-10-15 13:50:44 -0500

    Prorama final funcional

--- Programa_Factura.go
+++ Programa_Factura.go
@@ -69,40 +69,51 @@ func main() {
 	pdf := gofpdf.New("P", "mm", "A4", "")
 	pdf.AddPage()
 	pdf.SetFont("Arial", "B", 16)
-	pdf.Cell(0, 10, "Factura")
-	pdf.Ln(12)
+	pdf.CellFormat(0, 10, "Factura", "", 1, "C", false, 0, "")
+	pdf.Ln(10)
 
 	// Datos del cliente
+	pdf.SetFont("Arial", "B", 12)
+	pdf.Cell(20, 10, "Cliente:")
 	pdf.SetFont("Arial", "", 12)
-	pdf.Cell(0, 10, "Cliente: "+invoice.ClientName)
-	pdf.Ln(8)
+	pdf.Cell(0, 10, invoice.ClientName)
+	pdf.Ln(10)
+
 
 	// Fecha
-	pdf.Cell(0, 10, "Fecha: "+invoice.Date)
-	pdf.Ln(12)
+	pdf.SetFont("Arial", "B", 12)
+	pdf.Cell(20, 10, "Fecha:")
+	pdf.SetFont("Arial", "", 12)
+	pdf.Cell(0, 10, invoice.Date)
+	pdf.Ln(20)
+
+	pdf.SetLeftMargin(40)
+	pdf.SetFillColor(100, 100, 100) 
 
 	// Título de tabla
 	pdf.SetFont("Arial", "B", 12)
-	pdf.CellFormat(40, 10, "Producto", "1", 0, "C", false, 0, "")
-	pdf.CellFormat(30, 10, "Cantidad", "1", 0, "C", false, 0, "")
-	pdf.CellFormat(30, 10, "Precio Unitario", "1", 0, "C", false, 0, "")
-	pdf.CellFormat(40, 10, "Total", "1", 0, "C", false, 0, "")
-	pdf.Ln(-1)
+	pdf.CellFormat(40, 10, "Producto", "1", 0, "C", true, 0, "")
+	pdf.CellFormat(30, 10, "Cantidad", "1", 0, "C", true, 0, "")
+	pdf.CellFormat(40, 10, "Precio Unitario", "1", 0, "C", true, 0, "")
+	pdf.CellFormat(40, 10, "Total", "1", 1, "C", true, 0, "") // ln=1 para saltar a la siguiente línea
+
+	pdf.SetFillColor(255, 255, 255)
 
 	// Rellenar la tabla con productos
 	pdf.SetFont("Arial", "", 12)
 	for _, product := range invoice.Products {
 		pdf.CellFormat(40, 10, product.Name, "1", 0, "C", false, 0, "")
 		pdf.CellFormat(30, 10, fmt.Sprintf("%d", product.Quantity), "1", 0, "C", false, 0, "")
-		pdf.CellFormat(30, 10, fmt.Sprintf("%.2f", product.Price), "1", 0, "C", false, 0, "")
+		pdf.CellFormat(40, 10, fmt.Sprintf("%.2f", product.Price), "1", 0, "C", false, 0, "")
 		pdf.CellFormat(40, 10, fmt.Sprintf("%.2f", product.Price*float64(product.Quantity)), "1", 0, "C", false, 0, "")
 		pdf.Ln(-1)
 	}
 
-	// Total
-	pdf.SetFont("Arial", "B", 12)
+	pdf.SetLeftMargin(10) // Restablecer el margen izquierdo
 	pdf.Ln(10)
-	pdf.Cell(0, 10, "Total a pagar: "+fmt.Sprintf("%.2f", invoice.Total))
+	pdf.SetFont("Arial", "B", 12)
+	pdf.CellFormat(120, 10, "", "", 0, "", false, 0, "") // Espacio en blanco para desplazar el texto a la derecha
+	pdf.CellFormat(0, 10, "Total a pagar: "+fmt.Sprintf("%.2f", invoice.Total), "", 1, "R", false, 0, "") // Total alineado a la derecha
 
 	// Guardar el PDF
 	err := pdf.OutputFileAndClose("factura.pdf")
